58ab38e1070d29383581d94f4f1b14b4d87de269
VictorAraujo00 <gabrielpearaujo20@gmail.com>
2024-07-10 09:03:55 -0300

    Criando lógica para remover o mosquito

--- jogo.js
+++ jogo.js
@@ -10,6 +10,14 @@ ajustaTamanhoJogo()
 
 //Gerando posições aleatorias
 function posicaoRandomica(){
+
+	//Remover mosquito anterior (caso exista)
+
+	if(document.getElementById('mosquito')){
+		document.getElementById('mosquito').remove()
+	}
+
+
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -24,6 +32,7 @@ function posicaoRandomica(){
 	mosquito.style.position = 'absolute'
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
+	mosquito.id = 'mosquito'
 	document.body.appendChild(mosquito)
 }
 
